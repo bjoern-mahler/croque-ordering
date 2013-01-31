4bd7b51d57097dbd1cd91c61dccbdd450b34d4f7
bjoern.mahler <bjoern.mahler@lineas.de>
2013-01-31 09:03:20 +0100

    Paging and Sorting added

--- src/test/java/de/lineas/croqueorder/repository/MultipleOrderWithPaginationTest.java
+++ src/test/java/de/lineas/croqueorder/repository/MultipleOrderWithPaginationTest.java
@@ -16,14 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Some tests with multiple order objects configured in simple-data.json
+ * Some tests with paging and sorting of MealOrders
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:repository-context.xml", "classpath:test-pagination-data-context.xml"})
 @Transactional
-@DirtiesContext
+@DirtiesContext //cleans the dishes...
 public class MultipleOrderWithPaginationTest {
 
     @Autowired
@@ -37,15 +38,35 @@ public class MultipleOrderWithPaginationTest {
 
     @Test
     public void testLoadWithPaging() throws Exception {
-        Pageable pageable = new PageRequest(1, 20, new Sort("item"));
-        final Page<MealOrder> firstPage = orderRepository.findAll(pageable);
+        Pageable firstPageRequest = new PageRequest(0, 20);
+        final Page<MealOrder> firstPage = orderRepository.findAll(firstPageRequest);
+        assertTrue("should be the first page", firstPage.isFirstPage());
         assertEquals(20, firstPage.getContent().size());
-    }
 
+        Pageable secondPageRequest = new PageRequest(1, 20);
+        final Page<MealOrder> secondPage = orderRepository.findAll(secondPageRequest);
+        assertEquals(20, secondPage.getContent().size());
+        assertEquals(Long.valueOf(21L), secondPage.getContent().get(0).getId());
+
+        assertEquals(56, secondPage.getTotalElements());
+        assertEquals(3, secondPage.getTotalPages());
+    }
 
     @Test
     public void testLoadWithSorting() throws Exception {
         final List<MealOrder> sortedOrders= orderRepository.findAll(new Sort("amount"));
         assertEquals(56, sortedOrders.size());
+        assertEquals(1, sortedOrders.get(0).getAmount());
+        assertEquals(99, sortedOrders.get(55).getAmount());
+    }
+
+    @Test
+    public void testPagingWithSorting() throws Exception {
+        Pageable pageRequest = new PageRequest(2, 20, Sort.Direction.ASC, "amount");
+        final Page<MealOrder> lastPage = orderRepository.findAll(pageRequest);
+        assertTrue("should be the last page", lastPage.isLastPage());
+        assertEquals(56, lastPage.getTotalElements());
+        assertEquals(16, lastPage.getContent().size());
+        assertEquals(99, lastPage.getContent().get(56 - 2*20 - 1).getAmount());
     }
 }
